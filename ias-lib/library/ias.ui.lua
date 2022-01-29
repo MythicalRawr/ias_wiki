32461c8a3d8a0030e71d713762f3dab51d4b8ec7
failcake <failcake.work@gmail.com>
2022-01-29 12:13:37 +0100

    Add ui_keypad

--- ias-lib/library/ias.ui.lua
+++ ias-lib/library/ias.ui.lua
@@ -9,7 +9,7 @@ ui = {}
 
 ---
 ---@ENV CLIENT
----@param uiType string "frame, group, button, progress_bar, input, inventory_slot, label, image"
+---@param uiType string "frame, group, button, progress_bar, input, inventory_slot, label, image, keypad"
 ---@param parent? UIBase
 ---@return UIBase
 ---* Creates a new ui base element.
