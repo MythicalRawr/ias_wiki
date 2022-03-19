9178ab3b00f5fc9ea8d5944909b6c1e5e81973c4
failcake <failcake.work@gmail.com>
2022-03-19 11:29:32 +0100

    Add camera:isValid()

--- ias-lib/library/ias.camera.lua
+++ ias-lib/library/ias.camera.lua
@@ -9,7 +9,15 @@ camera = {}
 
 ---
 ---@ENV SHARED
+---@return boolean
+---* Returns true if the camera object is valid
+---
+function camera:isValid() end
+
+---
+---@ENV SHARED
 ---@param pos Vector3
+---@return Vector3
 ---* Converts the screen position to world coordinates
 ---
 function camera:toWorld(pos) end
@@ -17,6 +25,7 @@ function camera:toWorld(pos) end
 ---
 ---@ENV SHARED
 ---@param pos Vector3
+---@return Vector
 ---* Converts the world position to screen coordinates
 ---
 function camera:toScreen(pos) end
