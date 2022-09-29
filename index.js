9ce9fe3de28eb2396f7d57c253f13a320145ecf4
FailCake <edunad@users.noreply.github.com>
2022-09-29 21:49:57 +0200

    Fix index

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ const init = () => {
             class: classTemplate,
             extension: extensionTemplate,
         },
+        mdLinkParser: (type, outputFolder, data) => {
+            if (type === '$TITLE_NAME$') {
+                return `[${data.title.link}](${data.title.link.toLowerCase()})${data.title.msg.replace(data.title.link, '')}`;
+            } else if (type === '$PARAMETERS$' || type === '$RETURNS$' || type === '$FIELDS$') {
+                return `[${data.link}](${data.link.toLowerCase()})`;
+            } else if (type === 'SUMMARY') {
+                if (data.fileName) {
+                    // Not root
+                    return `[${data.name}](${outputFolder}/${data.dir}/${data.fileName.replace('.md', '').toLowerCase()})`;
+                } else {
+                    // ROOT
+                    return `[${data.dir}](${outputFolder}/${data.dir}/README)`;
+                }
+            }
+
+            throw new Error(`Unknown type: ${type}`);
+        },
     }).extract();
 };
 
