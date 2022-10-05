800a0dbeaac261d0411a88ad2d5497cc80697c8a
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2022-10-05 15:29:09 +0200

    add bone object

---
+++ ias-lib/library/ias.bone.lua
@@ -0,0 +1,61 @@
+---@meta
+
+---
+---@class Bone
+---* Bone's entry class
+Bone = {}
+
+---
+---@env CLIENT
+---@return Vector3
+---* Returns the custom position offset of the bone
+---
+function Bone:getPosition() end
+
+---
+---@env CLIENT
+---@param Vector3 "Custom offset"
+---* Sets the custom local position offset of the bone
+---
+function Bone:setPosition(position) end
+
+---
+---@env CLIENT
+---@return Vector3
+---* Returns the custom rotation offset of the bone in degrees
+---
+function Bone:getRotation() end
+
+---
+---@env CLIENT
+---@param Vector3 "Custom offset"
+---* Sets the custom position offset of the bone in degrees
+---
+function Bone:setRotation(rotation) end
+
+---
+---@env CLIENT
+---@return Vector3
+---* Returns the custom scale modifier of the bone
+---
+function Bone:getScale() end
+
+---
+---@env CLIENT
+---@param Vector3 "Custom modifier"
+---* Sets the custom scale modifier of the bone
+---
+function Bone:setScale(scale) end
+
+---
+---@env CLIENT
+---* Clears the position, rotation and scale modifiers for the bone.
+---
+function Bone:clear() end
+
+---
+---@env CLIENT
+---@return string
+---* Returns the bone name
+---
+function Bone:getName() end
