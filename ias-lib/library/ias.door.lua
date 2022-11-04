083a83bfd9a3a3c63b7cf746f1a359ebc68a0f5d
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2022-11-04 15:34:27 +0100

    add door file

---
+++ ias-lib/library/ias.door.lua
@@ -0,0 +1,14 @@
+---@meta
+
+---
+---@class Door : ENT
+---* door's entry class.
+---
+Door = {}
+
+---
+---@env SHARED
+---@return boolean
+---* Returns the state of the door
+---
+function Door:isOpen() end
