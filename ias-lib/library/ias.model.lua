799c6b07cc838bfbc7ed020fa2f8ea083f164c4d
Bromulus Vliegius <bromvliegbrombeer@gmail.com>
2023-03-03 14:13:56 +0100

    format & template functions add

--- ias-lib/library/ias.model.lua
+++ ias-lib/library/ias.model.lua
@@ -11,7 +11,16 @@ Model = {}
 ---@return boolean
 ---* Returns true if the model is valid
 ---
-function Model:isValid() end
+function Model:isValid()
+end
+
+---
+---@env SHARED
+---@return string
+---* Returns the path of the model
+---
+function Model:getFileName()
+end
 
 ---
 ---@env SHARED
@@ -19,18 +28,21 @@ function Model:isValid() end
 ---@return ModelMesh
 ---* Returns the model mesh if found
 ---
-function Model:getMesh(name) end
+function Model:getMesh(name)
+end
 
 ---
 ---@env SHARED
 ---@param mesh ModelMesh
 ---* Adds the mesh to the model
 ---
-function Model:addMesh(mesh) end
+function Model:addMesh(mesh)
+end
 
 ---
 ---@env SHARED
 ---@return ModelMesh[]
 ---* Returns all the model meshes
 ---
-function Model:getMeshes() end
+function Model:getMeshes()
+end
